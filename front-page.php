523e490355811466ef7eab89eb0bfd4cea726cea
Pau <pau.pulidocompanys@gmail.com>
2022-09-20 14:10:48 +0200

    front page and navigation set-up

---
+++ front-page.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Fornt-page elmercatcultural.cat
+ *
+ * This is the template for the front-page for the elmercatcultural project
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package elmercatcultural.cat
+ */
+get_header();
+?>
+<main class="front-page">
+    <section id="cover" class="front-page__section">
+        <h1>PORTADA</h1>
+    </section>
+    <section id="destacats" class="front-page__section">
+        <h1>DESTACATS</h1>
+    </section>
+    <section id="participa" class="front-page__section">
+        <h1>PARTICIPA</h1>
+    </section>
+</main>
+<?php get_footer(); ?>
