97cfaf37ff20bbb7e88d74ff9171ddc3a26a348d
Lucas García <lucas@codeccoop.org>
2022-10-05 16:00:37 +0200

    Footer content

--- footer.php
+++ footer.php
@@ -18,7 +18,7 @@
             <img class="footer__logo" src="<?= get_bloginfo('template_url') . '/assets/images/logo--medium.png'; ?>">
         </div>
         <div class="footer__column">
-            <div class="footer__column__adress">
+            <div class="footer__row">
                 <p class="footer__title small">
                     Contacte
                 </p>
@@ -26,7 +26,7 @@
                     <?= preg_replace('/\n/', '<br>', get_theme_mod('contact')) ?>
                 </p>
             </div>
-            <div class="footer__column__timetable">
+            <div class="footer__row">
                 <p class="footer__title small">
                     Horari
                 </p>
@@ -36,19 +36,46 @@
             </div>
 
         </div>
-        <div class="footer__column"></div>
         <div class="footer__column">
-            <?php
-            $coordinates = array_map('trim', explode(' ', get_theme_mod('coordinates')));
-            # $coordinates = [41.5028, 1.81346];
-            $lat = $coordinates[0];
-            $lng = $coordinates[1];
-            echo do_shortcode('[embedded_map lng="' . $lng . '" lat="' . $lat . '" class="contact__map"]');
-            ?>
+            <div class="footer__row">
+                <p class="footer__title small">Dona't d'alta al nostre butlletí</p>
+            </div>
         </div>
-        <div class="footer__column"></div>
-        <div class="footer__column"></div>
-        <div class="footer__column"></div>
+        <div class="footer__column">
+            <div class="footer__row">
+                <p class="footer__title small">Com arribar</p>
+            </div>
+            <div class="footer__row">
+                <?php
+                $coordinates = array_map('trim', explode(' ', get_theme_mod('coordinates')));
+                # $coordinates = [41.5028, 1.81346];
+                $lat = $coordinates[0];
+                $lng = $coordinates[1];
+                echo do_shortcode('[embedded_map lng="' . $lng . '" lat="' . $lat . '" class="contact__map"]');
+                ?>
+            </div>
+        </div>
+        <div class="footer__column">
+            <div class="footer__row">
+                <p class="footer__title small">Troba-ns a</p>
+                <ul class="footer__social">
+                    <li><a></a></li>
+                    <li><a></a></li>
+                    <li><a></a></li>
+                    <li><a></a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="footer__column">
+            <div class="footer__row">
+                <p class="footer__text small"><a>Avís legal</a></p>
+                <p class="footer__text small"><a>Política de privacitat</a></p>
+            </div>
+            <div class="footer__row">
+                <p class="footer__text small">Amb el suport de</p>
+                <img src="<?= get_bloginfo('template_url') . '/assets/images/logo-ajuntament.png'; ?>" />
+            </div>
+        </div>
     </div>
     <div class="site-info">
         Pàgina web feta amb <a href="<?php echo esc_url(__('https://wordpress.org/', 'elmercatcultural.cat')); ?>">WordPress</a>
