1befa1947d231e6a35ae2b65d7016b73aaab0c3d
Lucas García <lucas@codeccoop.org>
2022-10-05 15:09:43 +0200

    Clamp highlight text with spans

--- js/front-page.js
+++ js/front-page.js
@@ -1,11 +1,62 @@
 document.addEventListener( 'DOMContentLoaded', function() {
+	for ( const caption of document.querySelectorAll(
+		'.front-page__jumbotron-item figcaption'
+	) ) {
+		spanLines( caption );
+	}
+
 	const jumbo = jQuery( '.front-page__jumbotron' );
 	if ( jumbo.hasClass( 'slider' ) ) {
+		jumbo.on( 'init', function( ev, slick ) {
+			slick.slickPlay();
+		} );
 		jumbo.slick( {
 			autoplay: true,
+			draggable: true,
 			autoplaySpeed: 3000,
 			arrows: false,
 			dots: true,
+			mobileFirst: true,
 		} );
+	}
+
+	function spanLines( el ) {
+		const nodeLines = Array.from( el.childNodes )
+			.filter( ( n ) => n.nodeType === 3 )
+			.reduce( ( acum, node ) => {
+				const range = document.createRange();
+				const lines = [];
+				const str = node.textContent;
+
+				range.setStart( node, 0 );
+
+				let prevBottom = range.getBoundingClientRect().bottom;
+				let current = 1;
+				let lastFound = 0;
+				let bottom = 0;
+
+				while ( current <= str.length ) {
+					range.setStart( node, current );
+					if ( current < str.length - 1 ) {
+						range.setEnd( node, current + 1 );
+					}
+					bottom = range.getBoundingClientRect().bottom;
+					if ( bottom > prevBottom ) {
+						acum.push( str.substr( lastFound, current - lastFound ) );
+						prevBottom = bottom;
+						lastFound = current;
+					}
+					current++;
+				}
+
+				acum.push( str.substr( lastFound ) );
+
+				return acum;
+			}, [] );
+
+		el.innerHTML = '';
+		for ( const line of nodeLines ) {
+			el.innerHTML += '<span>' + line + '</span>';
+		}
 	}
 } );
