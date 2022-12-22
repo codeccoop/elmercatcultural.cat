ce5f3003ba15bc257f0d33c54e7d01e209834ee4
Pau <pau@codeccoop.org>
2022-12-22 09:44:03 +0100

    fix: create product conditional

--- functions.php
+++ functions.php
@@ -433,7 +433,7 @@ function new_radio_field( $checkout ) {
 add_filter('wp_insert_post_data', 'elmercatcultural_on_event_insert', 99, 2);
 function elmercatcultural_on_event_insert($data, $postarr)  // , $unsanitized_postarr = null, $update = false)
 {
-    if ($postarr['post_type'] === 'event' || $postarr['post_type'] === 'workshop' && $postarr['ID'] != 0 && $data['post_status'] != 'trash') {
+    if (($postarr['post_type'] === 'event' || $postarr['post_type'] === 'workshop') && $postarr['ID'] != 0 && $data['post_status'] != 'trash') {
         $slug = wp_unique_post_slug($postarr['post_title'], $postarr['ID'], $postarr['post_status'], $postarr['post_type'], null);
       
         $product = elmercatcultural_find_product_by_slug($slug);
