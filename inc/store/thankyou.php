d40ca0f14305518f96b831f193e62733f06bdfdd
Pau <pau@codeccoop.org>
2023-09-07 12:00:35 +0200

    feat: force presencial payment orders to complete and logo

--- inc/store/thankyou.php
+++ inc/store/thankyou.php
@@ -1,7 +1,8 @@
 <?php
 add_filter('woocommerce_thankyou_order_received_text', 'elmercatcultural_thankyou_text');
-function elmercatcultural_thankyou_text($text, $order = null)
+function elmercatcultural_thankyou_text()
 {
+
 	return 'Benvolgudes, gràcies per realitzar la inscripció a elMercat';
 }
 
@@ -10,3 +11,35 @@ function elmercatcultural_thankyou_text($text, $order = null)
 /* { */
 /* 	return $text; */
 /* } */
+
+/**
+ * Auto Complete Pagament en Efectiu WooCommerce orders.
+ */
+// add_action('woocommerce_thankyou', 'custom_woocommerce_auto_complete_order');
+// function custom_woocommerce_auto_complete_order($order_id)
+// {
+// 	if (!$order_id) {
+// 		return;
+// 	}
+
+// 	$order = wc_get_order($order_id);
+// 	$order->update_status('completed');
+// }
+
+
+add_action('woocommerce_before_thankyou', 'cod_order_payment_processing_order_status');
+
+function cod_order_payment_processing_order_status($order_id)
+{
+	if (!$order_id) {
+		return;
+	}
+
+	// Get an instance of the WC_Order object
+	//throw new Exception(print_r($order_id));
+	$order = new WC_Order($order_id);
+
+	if ($order->get_payment_method() == 'cod') {
+		$order->update_status('completed');
+	}
+}
