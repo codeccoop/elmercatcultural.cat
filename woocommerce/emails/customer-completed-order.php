269574a5882a876224da819ed775901aa316d3d3
Lucas García <lucas@codeccoop.org>
2024-12-08 22:59:15 +0100

    feat: custom wc email tempaltes

--- woocommerce/emails/customer-completed-order.php
+++ woocommerce/emails/customer-completed-order.php
@@ -26,8 +26,10 @@ do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 
 <?php /* translators: %s: Customer first name */ ?>
 <p><?php printf( esc_html__( 'Hi %s,', 'woocommerce' ), esc_html( $order->get_billing_first_name() ) ); ?></p>
-<p><?php esc_html_e( 'We have finished processing your order.', 'woocommerce' ); ?></p>
+<p><?php esc_html_e( 'Hem rebut el teu pagament i la teva inscripció a la següent activitat de elMercat està confirmada.', 'woocommerce' ); ?></p>
 <?php
+
+
 
 /*
  * @hooked WC_Emails::order_details() Shows the order details table.
@@ -43,12 +45,6 @@ do_action( 'woocommerce_email_order_details', $order, $sent_to_admin, $plain_tex
 do_action( 'woocommerce_email_order_meta', $order, $sent_to_admin, $plain_text, $email );
 
 /*
- * @hooked WC_Emails::customer_details() Shows customer details
- * @hooked WC_Emails::email_address() Shows email address
- */
-do_action( 'woocommerce_email_customer_details', $order, $sent_to_admin, $plain_text, $email );
-
-/**
  * Show user-defined additional content - this is set in each email's settings.
  */
 if ( $additional_content ) {
@@ -56,6 +52,13 @@ if ( $additional_content ) {
 }
 
 /*
+ * @hooked WC_Emails::customer_details() Shows customer details
+ * @hooked WC_Emails::email_address() Shows email address
+ */
+do_action( 'woocommerce_email_customer_details', $order, $sent_to_admin, $plain_text, $email );
+
+/**
+
  * @hooked WC_Emails::email_footer() Output the email footer
  */
 do_action( 'woocommerce_email_footer', $email );
