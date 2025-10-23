68f32b37743f23ee4f5c8e104a180b059337fa89
Lucas García <lucas@codeccoop.org>
2025-10-23 12:10:05 +0200

    feat: emc_get_post_date_field

--- ajax/async-grid.php
+++ ajax/async-grid.php
@@ -1,95 +1,94 @@
 <?php
 
-add_action('wp_ajax_get_grid_items', 'emc_get_grid_items');
-add_action('wp_ajax_nopriv_get_grid_items', 'emc_get_grid_items');
+add_action( 'wp_ajax_get_grid_items', 'emc_get_grid_items' );
+add_action( 'wp_ajax_nopriv_get_grid_items', 'emc_get_grid_items' );
 
-if (!function_exists('emc_get_grid_items')) {
-    function emc_get_grid_items()
-    {
-        check_ajax_referer('async_grid');
-        $term = $_POST['term'];
-        $page = $_POST['page'];
-        $type = $_POST['type'];
-        $order = $term === 'historic' ? 'DESC' : 'ASC';
-        $time_dir = $term === 'historic' ? '<=' : '>=';
+if ( ! function_exists( 'emc_get_grid_items' ) ) {
+	function emc_get_grid_items() {
+		check_ajax_referer( 'async_grid' );
+		$term     = sanitize_text_field( wp_unslash( $_POST['term'] ?? 'all' ) );
+		$page     = intval( wp_unslash( $_POST['page'] ?? '1' ) );
+		$type     = sanitize_text_field( wp_unslash( $_POST['type'] ?? '' ) );
+		$order    = 'historic' === $term ? 'DESC' : 'ASC';
+		$time_dir = 'historic' === $term ? '<=' : '>=';
 
-        $args = [
-            'post_type' => $type,
-            'post_status' => 'publish',
-            'posts_per_page' => 9,
-            'offset' => ($page - 1) * 9,
-            'meta_key' => 'date',
-            'orderby' => 'meta_value',
-            'order' => $order,
-            'meta_query' => [
-                [
-                    'key' => 'date',
-                    'compare' => $time_dir,
-                    'value' => date('Y-m-d'),
-                    'type' => 'DATE',
-                ]
-            ]
-        ];
+		$args = array(
+			'post_type'      => $type,
+			'post_status'    => 'publish',
+			'posts_per_page' => 9,
+			'offset'         => ( $page - 1 ) * 9,
+			'meta_key'       => 'date',
+			'orderby'        => 'meta_value',
+			'order'          => $order,
+			'meta_query'     => array(
+				array(
+					'key'     => 'date',
+					'compare' => $time_dir,
+					'value'   => date( 'Y-m-d' ),
+					'type'    => 'DATE',
+				),
+			),
+		);
 
-        if ($term != 'all' && $term != 'historic') {
-            $args['category_name'] = $term;
-        }
+		if ( 'all' !== $term && 'historic' !== $term ) {
+			$args['category_name'] = $term;
+		}
 
-        $query = new WP_Query($args);
+		$query = new WP_Query( $args );
 
-        $data = [
-            'posts' => [],
-            'pages' => 0
-        ];
+		$data = array(
+			'posts' => array(),
+			'pages' => 0,
+		);
 
-        while ($query->have_posts()) {
-            $query->the_post();
-            $ID = get_the_ID();
-            $thumbnail = get_the_post_thumbnail_url($ID, 'full');
-            if (!$thumbnail) {
-                $thumbnail = get_template_directory_uri() . '/assets/images/event--default.png';
-            }
+		while ( $query->have_posts() ) {
+			$query->the_post();
+			$post_id   = get_the_ID();
+			$thumbnail = get_the_post_thumbnail_url( $post_id, 'full' );
+			if ( ! $thumbnail ) {
+				$thumbnail = get_template_directory_uri() . '/assets/images/event--default.png';
+			}
 
-            $now = current_time('U', false);
-            try {
-                $event_date = (DateTime::createFromFormat('d/m/Y g:i a', get_field('date', $ID)))->getTimestamp();
-                $has_inscription = get_field('checkbox', $ID);
-                $date_sale_from = $has_inscription
-                    ? (DateTime::createFromFormat('d/m/Y g:i a', get_field('date_sale_from', $ID)))->getTimestamp()
-                    : null;
-                $date_sale_to = $has_inscription
-                    ? (DateTime::createFromFormat('d/m/Y g:i a', get_field('date_sale_to', $ID)))->getTimestamp()
-                    : null;
-            } catch (Error $e) {
-                $date_sale_from = (DateTime::createFromFormat('d/m/Y g:i a', get_field('date', $ID)))->getTimestamp();
-                $date_sale_to = (DateTime::createFromFormat('d/m/Y g:i a', get_field('date', $ID)))->getTimestamp();
-            }
+			$now = current_time( 'U', false );
+			try {
+				$event_date      = emc_get_date_field( 'date', $post_id )->getTimestamp();
+				$has_inscription = get_field( 'checkbox', $post_id );
+				$date_sale_from  = $has_inscription
+					? emc_get_date_field( 'date_sale_from', $post_id )->getTimestamp()
+					: null;
+				$date_sale_to    = $has_inscription
+					? emc_get_date_field( 'date_sale_to', $post_id )->getTimestamp()
+					: null;
+			} catch ( Error ) {
+				$date_sale_from = emc_get_date_field( 'date', $post_id )->getTimestamp() ?? null;
+				$date_sale_to   = emc_get_date_field( 'date', $post_id )->getTimestamp() ?? null;
+			}
 
-            $isopen = true;
-            if ($date_sale_from && $date_sale_to) {
-                $isopen = $date_sale_from < $now && $date_sale_to > $now && $event_date > $now;
-            } elseif (!get_field('checkbox', $ID)) {
-                $isopen = $event_date > $now;
-            }
+			$isopen = true;
+			if ( $date_sale_from && $date_sale_to ) {
+				$isopen = $date_sale_from < $now && $date_sale_to > $now && $event_date > $now;
+			} elseif ( ! get_field( 'checkbox', $post_id ) ) {
+				$isopen = $event_date > $now;
+			}
 
-            $data['posts'][] = [
-                'id' => $ID,
-                'title' => get_the_title($ID),
-                'category' => get_the_category($ID),
-                'excerpt' => get_the_excerpt($ID),
-                'url' => get_post_permalink($ID),
-                'thumbnail' => $thumbnail,
-                'hour' => get_field('hour', $ID),
-                'available_stock' => get_field('available_stock', $ID),
-                'isopen' => $isopen,
-                'checkbox' => get_field('checkbox', $ID),
-            ];
-        }
+			$data['posts'][] = array(
+				'id'              => $post_id,
+				'title'           => get_the_title( $post_id ),
+				'category'        => get_the_category( $post_id ),
+				'excerpt'         => get_the_excerpt( $post_id ),
+				'url'             => get_post_permalink( $post_id ),
+				'thumbnail'       => $thumbnail,
+				'hour'            => get_field( 'hour', $post_id ),
+				'available_stock' => get_field( 'available_stock', $post_id ),
+				'isopen'          => $isopen,
+				'checkbox'        => get_field( 'checkbox', $post_id ),
+			);
+		}
 
-        $count = $query->found_posts;
-        $pages = ceil($count / 9);
-        $data['pages'] = $pages;
+		$count         = $query->found_posts;
+		$pages         = ceil( $count / 9 );
+		$data['pages'] = $pages;
 
-        wp_send_json($data, 200);
-    }
+		wp_send_json( $data, 200 );
+	}
 }
